2c8014e82cf6d2918f29301a6e9d13fb4102a35d
gabmor38 <Gabriela.morenor@gmail.co>
2022-01-13 22:31:16 -0500

    implemented about us page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -24,7 +26,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    get 'about/' => 'about#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
