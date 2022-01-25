8ef4080e475731cc1095d6b0ec58ecee24636a14
gabmor38 <Gabriela.morenor@gmail.co>
2022-01-24 22:17:26 -0500

    fixed testing products

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,59 +2,34 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-
-    it "is valid" do
-      @product = Product.new
-      @category = Category.new
-      @category.name= 'Category_Test'
-      @category.save!
-      @product.name = 'Product_Test' # invalid state
-      @product.price_cents = 1234
-      @product.quantity = 500
-      @product.category_id = @category.id
-      @product.save!
-      expect(@product.valid?).to be true
+    category = Category.create! name: 'Apparel'
+    product = category.products.create!({
+      name: 'Hipster Hat',
+      quantity: 4,
+      price_cents: 23
+    })
+    it 'should create a new product successfully' do
+      expect(product).to be_valid
     end
-
-    it "name is present" do
-      @product = Product.new
-      @product.name = nil
-      @product.valid?
-      expect(@product.errors[:name]).to include("can't be blank")
-  
-      @product.name = 'Product_Test' # this is the product.name in the valid state
-      @product.valid? 
-      expect(@product.errors[:name]).not_to include("can't be blank")
+    it 'should have a name' do
+      product.name = nil
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Name can't be blank")
     end
-
-    it "price_cents is present" do
-      @product = Product.new
-      @product.price_cents = nil # invalid state
-      @product.valid?
-      expect(@product.errors[:price_cents]).to include("is not a number")
-  
-      @product.price_cents = 1234 # valid state
-      @product.valid? 
-      expect(@product.errors[:price_cents]).not_to include("can't be blank")
+    it 'should have a price' do
+      product.price_cents = nil
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Price can't be blank")
     end
-
-    it "quantity is present" do
-      @product = Product.new
-      @product.quantity = nil
-      @product.valid?
-      expect(@product.errors[:quantity]).to include("can't be blank")
-
-      @product.quantity = 500
-      @product.valid?
-      expect(@product.errors[:quantity]).not_to include("can't be blank")
+    it 'should have a quantity' do
+      product.quantity = nil
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Quantity can't be blank")
     end
-
-    xit "category is present" do
-      @category = Category.new
-      @product.category_id = nil 
-      @product.valid?
-      expect(@product.errors[:category]).to include("can't be blank")
-
+    it 'should have a category' do
+      product.category_id = nil
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Quantity can't be blank")
     end
   end
 end
