bc5a4c47b35fec6689317aa6707ac42b69fb7365
gabmor38 <Gabriela.morenor@gmail.co>
2022-01-30 16:12:18 -0500

    click on a product test complete

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  
+  # SETUP
+  before :each do
+  @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "Navigates to the product by clicking on a product" do
+    visit root_path
+
+    # DEBUG / VERIFY
+    first('a.btn.btn-default').click
+
+    save_screenshot
+  end
+end
